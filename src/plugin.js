f6b87adce386302200435608a943ad7145a058af
toolbuilder <k_d_hudson@yahoo.com>
2020-05-23 10:08:57 -0400

    added description to package.json

--- src/plugin.js
+++ src/plugin.js
@@ -2,7 +2,8 @@ import fs from 'fs-extra'
 import { join } from 'path'
 import pkgDir from 'pkg-dir'
 
-// Create the dependency version string for the pack file being tested
+// Create the dependency version string for the pack file being tested.
+// This is how npm will name the pack file when 'npm pack' is called.
 const packFileDependency = (packageJson) => {
   const name = packageJson.name.replace('@', '').replace('/', '-')
   const version = packageJson.version
@@ -21,7 +22,8 @@ const collectDependencies = (bundle) => {
 }
 
 /*
-  Merge dependency version information from packageJson with dependency ids
+  Merge dependency version information from packageJson with
+  dependency ids from Rollup bundle generation.
   Parameter testDependencies is just an array of external package names
 */
 const buildTestDependencies = (packageJson, testDependencies) => {
@@ -39,6 +41,24 @@ const readPackageJson = async (rootDir) => {
   return fs.readJSON(join(path, 'package.json'))
 }
 
+const createTestPackageJson = (testPackageJson, packageJson, bundle) => {
+  return {
+    name: `${packageJson.name}-package-test`,
+    version: '1.0.0',
+    description: `Generated package test for ${packageJson.name}`,
+    main: 'index.js',
+    scripts: {},
+    author: 'rollup-plugin-test-package-json',
+    devDependencies: {},
+    ...testPackageJson,
+    dependencies: {
+      ...buildTestDependencies(packageJson, collectDependencies(bundle)),
+      [packageJson.name]: packFileDependency(packageJson), // for some reason, not picking this up from Rollup
+      ...testPackageJson.dependencies // we've already ensured that this exists
+    }
+  }
+}
+
 export default (userOptions = {}) => {
   const options = {
     rootDir: undefined, // where to find packageJson if not provided
@@ -50,37 +70,24 @@ export default (userOptions = {}) => {
     name: 'create-test-package-json',
 
     async renderStart (outputOptions, inputOptions) {
-      options.packageJson = options.packageJson || await readPackageJson(options.rootDir)
-      options.testPackageJson = {
-        name: `${options.packageJson.name}-package-test`,
-        version: '1.0.0',
-        description: `Generated package test for ${options.packageJson.name}`,
-        main: 'index.js',
-        scripts: {},
-        author: 'rollup-plugin-test-package-json',
-        dependencies: {},
-        devDependencies: {},
-        ...options.testPackageJson
+      try {
+        options.packageJson = options.packageJson || await readPackageJson(options.rootDir)
+      } catch (error) {
+        this.error(`Problem loading package.json, check 'rootDir' option which is: ${options.rootDir}. Message ${error.message}`)
       }
     },
 
     async generateBundle (outputOptions, bundle, isWrite) {
-      if (!isWrite) this.error('create-test-package-json only works when writing to disk')
-      const packageJson = options.packageJson
-      const testPackageJson = options.testPackageJson
-      testPackageJson.dependencies = {
-        ...buildTestDependencies(packageJson, collectDependencies(bundle)),
-        [packageJson.name]: packFileDependency(packageJson), // for some reason, not picking this up from Rollup
-        ...testPackageJson.dependencies
-      }
+      if (!isWrite) this.error('This package needs to write to disk, and Rollup is not writing to disk with this configuration')
+      const testPackageJson = { dependencies: {}, ...options.testPackageJson } // ensure it has dependencies Object
+      options.testPackageJson = createTestPackageJson(testPackageJson, options.packageJson, bundle)
     },
 
-    async renderError () { /* Nothing to do */ },
+    // renderError not needed
 
     async writeBundle (outputOptions, chunks) {
-      const testPackageJson = options.testPackageJson
-      const testDir = outputOptions.dir
-      await fs.writeJSON(join(testDir, 'package.json'), testPackageJson, { spaces: 2 })
+      const path = join(outputOptions.dir, 'package.json')
+      await fs.writeJSON(path, options.testPackageJson, { spaces: 2 })
     }
   }
 }
